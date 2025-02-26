d412f219f83d79e3e250f909f927a20c46c6805e
Narendrakumar-Suresh <138976636+Narendrakumar-Suresh@users.noreply.github.com>
2025-02-26 13:44:19 +0530

    type command with path

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 import sys
+import os
+
+def find_executable(command, path_dirs):
+    """Search for an executable in the PATH directories."""
+    for directory in path_dirs:
+        full_path = os.path.join(directory, command)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+    return None
 
 def main():
     builtin=['echo','exit','type']
+    path_variable = os.environ.get("PATH", "")
+    path_dirs = path_variable.split(":") if path_variable else []
     while True:    
         sys.stdout.write("$ ")
         # Wait for user input
@@ -15,11 +26,16 @@ def main():
             case "echo":
                 print(" ".join(args))
             case 'type':
+                if not args:
+                    continue
                 args="".join(args)
-                if args in builtin:
-                    print(f"{args} is a shell builtin")
+                executable_path = find_executable(args, path_dirs)
+
+                if executable_path:
+                        print(f"{args} is {executable_path}")
                 else:
-                    sys.stdout.write(f"{args}: not found\n")
+                    print(f"{args}: not found")
+
             case _:
                 sys.stdout.write(f"{cmd}: command not found\n")
 
